c9286c5b9db9c7062cf77204f21496f835cd53b8
Ezequiel Etchevest <e.etchevest@hotmail.com>
2023-07-06 10:46:53 +0200

    axios

--- api/dist/components/index.js
+++ api/dist/components/index.js
@@ -2,16 +2,10 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
 Object.defineProperty(exports, "__esModule", { value: true });
-
 const express_1 = require("express");
-
 const sales_routes_1 = __importDefault(require("./sales/sales.routes"));
 const cambio = "hola";
-
 const mainRouter = (0, express_1.Router)();
-
 mainRouter.use('/api/sales', sales_routes_1.default);
-
 exports.default = mainRouter;
--- api/dist/components/sales/sales.controller.js
+++ api/dist/components/sales/sales.controller.js
@@ -12,11 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-
 exports.getSales = void 0;
-
 const db_1 = __importDefault(require("../../db"));
-
 function getSales(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
