57a14fb0d00b2c76125bdbcab272eb142668b44a
AritaRuv <ariadnaruvini@hotmail.com.ar>
2023-07-07 12:52:37 +0200

    product card

--- client/src/app/components/homeProducts.tsx/_homeProductContainer.tsx
+++ client/src/app/components/homeProducts.tsx/_homeProductContainer.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Box, Flex, SimpleGrid } from '@chakra-ui/react';
+import { Box, Flex, SimpleGrid, useMediaQuery } from '@chakra-ui/react';
 import ProductCard from '../productCard/_productCard';
 
 const HomeProductContainer: React.FC = () => {
+  const [isSmallScreen] = useMediaQuery("(max-width: 900px)");
+  const [isExtraSmallScreen] = useMediaQuery("(max-width: 480px)");
+
+  let gridColumns = 4;
+  if (isSmallScreen) {
+    gridColumns = 2;
+  }
+  if (isExtraSmallScreen) {
+    gridColumns = 1;
+  }
+
   return (
     <SimpleGrid
       spacingY={2}
       py={'2%'}
       px={'4%'}
-      minChildWidth={'160px'}
       border={'2px solid red'}
       w={'100vw'}
       placeItems={'center'}
+      columns={gridColumns} // Establece el número de columnas dinámicamente
     >
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </SimpleGrid>
   );
 };
--- client/src/app/components/productCard/_productCard.tsx
+++ client/src/app/components/productCard/_productCard.tsx
@@ -1,17 +1,91 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
-import NextImage from 'next/image'
+import { Box, Popover, PopoverTrigger, PopoverContent, IconButton, Text, Button } from '@chakra-ui/react';
+import NextImage from 'next/image';
+import { useState } from 'react';
+import { PiCaretDownThin } from 'react-icons/pi'
+import '../../assets/styleSheet.css';
+import { Bowlby_One_SC } from 'next/font/google';
 
 const ProductCard: React.FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [disableBox, setDisableBox] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsDropdownOpen(true);
+    setDisableBox(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDropdownOpen(false);
+    setDisableBox(false);
+  };
+
+
 
   return (
-    <Box
-      border={'2px solid red'}
-      w={'16vw'}
-      h={'42vh'}
-      minW={'160px'}
-      >
-          {/* <NextImage objectFit='contain' fill src={'https://naturalistone-images.s3.amazonaws.com/Limestone/Nuoro/Nuoro_0.jpg'} alt='img'/> */}
+    <Box position="relative">
+      <Popover isOpen={isDropdownOpen}>
+        <PopoverTrigger>
+          <Box
+            w={'14vw'}
+            h={'42vh'}
+            minW={'200px'}
+            position={'relative'}
+            overflow={'hidden'}
+            rounded={'md'}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <NextImage objectFit="cover" fill src={'https://naturalistone-images.s3.amazonaws.com/Limestone/Nuoro/Nuoro_0.jpg'} alt="img" />
+            <Box
+              display={'flex'}
+              w={'14vw'}
+              minW={'200px'}
+              bg={'rgba(210, 210, 210, 0.3)'}
+              h={'5vh'}
+              position={'absolute'}
+              bottom={0}
+              left={0}
+              hidden={disableBox}
+              placeContent={'center'}
+            >
+            <IconButton
+              display={'flex'}
+              placeContent={'center'}
+              icon={<PiCaretDownThin/>}
+              variant={'unstyled'}
+              size={'lg'}
+              aria-label={'Description'}/>
+            </Box>
+          </Box>
+        </PopoverTrigger>
+        <PopoverContent
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          rounded={'none'}
+          border={'none'}
+          position="relative"
+          h={'8vh'}
+          w={'14vw'}
+          minW={'200px'}
+          zIndex={10}
+          mt={'-9vh'}
+          className="custom-popover"
+          bg={'rgba(210, 210, 210, 0.5)'}
+        >
+        <Box>
+          {/* <Box>
+          <Text>Material</Text>
+          <Button variant={'unstyled'}>Nombre del producto</Button>
+          </Box>
+          <Box>
+          <Button variant={'unstyled'}>ORDER SAMPLE</Button>
+          <Button variant={'unstyled'}>ADD TO CART</Button>
+          </Box> */}
+
+        </Box>
+        </PopoverContent>
+      </Popover>
     </Box>
   );
 };
