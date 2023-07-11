6d8fe425960b6ce74b7b64fa13fd9fc68f60dd32
Eduardo Sequeira <eduardoasm19@gmail.com>
2023-07-11 19:31:10 -0400

    PUBLIC123-10

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -14,16 +14,6 @@ const cards = [
     name: "Nombre Honed",
     img: "https://images.unsplash.com/photo-1582913130063-8318329a94a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
   },
-  // "https://images.unsplash.com/photo-1571432248690-7fd6980a1ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-];
-
-const cards1 = [
-  {
-    material: "Honed",
-    name: "Nombre Honed",
-    // img: "https://thumbs.gfycat.com/IdealUnknownAlaskanhusky-mobile.mp4",
-  },
-  // "https://media3.giphy.com/media/MUDBH3wJIOnHAtE3xM/giphy.gif?cid=ecf05e47asx5sp9gszouug0hy3nuavahme3lup8yrlf6pm8v&ep=v1_gifs_related&rid=giphy.gif&ct=g",
 ];
 
 export default function Home() {
