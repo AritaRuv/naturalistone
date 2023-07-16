189033f5edb03f5dd29293646d0d2157350c0f3e
Eduardo Sequeira <eduardoasm19@gmail.com>
2023-07-16 19:33:17 -0400

    changes

--- api/src/components/index.ts
+++ api/src/components/index.ts
@@ -2,14 +2,12 @@ import { Router } from "express";
 import salesRouter from "./sales/sales.routes";
 import productsRouter from "./products/products.routes";
 import cartRouter from "./cart/cart.routes";
-import dimensionsRouter from "./dimensions/dimensions.routes";
 import colorsRouter from "./colors/colors.routes";
 const mainRouter = Router();
 
 mainRouter.use("/api/sales", salesRouter);
 mainRouter.use("/api/products", productsRouter);
 mainRouter.use("/api/cart", cartRouter);
-mainRouter.use("/api/dimensions", dimensionsRouter);
 mainRouter.use("/api/colors", colorsRouter);
 
 export default mainRouter;
