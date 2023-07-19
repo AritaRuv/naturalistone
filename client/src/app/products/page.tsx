e2730eb37cacefccd5998e0376fe98535bf77a02
AritaRuv <ariadnaruvini@hotmail.com.ar>
2023-07-19 13:35:11 +0200

    materials

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -10,7 +10,7 @@ export default function Products() {
     <>
       <NavBar />
       <MaterialFilter/>
-      {/* <Grid
+      <Grid
         h={'92.5vh'}
         templateRows="1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr" // 3 filas en el eje y
         templateColumns="1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr" // 3 columnas en el eje x
@@ -18,7 +18,7 @@ export default function Products() {
         <GridItem bg={'blue'} rowSpan={2} colSpan={8}></GridItem>
         <GridItem bg={'red'} rowSpan={6} colSpan={1} ></GridItem>
         <GridItem bg={'yellow'} rowSpan={6} colSpan={7} ></GridItem>
-      </Grid> */}
+      </Grid>
     </>
   );
 }
