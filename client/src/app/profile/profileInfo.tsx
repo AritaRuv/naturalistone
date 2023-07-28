30dcda77a77b7f1007d3e6148194d278dff094df
AritaRuv <ariadnaruvini@hotmail.com.ar>
2023-07-28 10:22:20 +0200

    src

--- client/src/app/profile/profileInfo.tsx
+++ client/src/app/profile/profileInfo.tsx
@@ -1,25 +1,25 @@
-'use client'
-import { Avatar, Box, useMediaQuery, Text } from "@chakra-ui/react";
+"use client";
+import { Box, Text } from "@chakra-ui/react";
 import { IShowMenu } from "./page";
 import SideCard from "./sideCard";
 
 
-const ProfileInfo: React.FC<IShowMenu> = ({setShowMenu, showMenu}) => {
+const ProfileInfo: React.FC<IShowMenu> = () => {
   
   return (
     <>
-    <Box pl={'5vw'}  w={'70vw'}>
-      <Text textTransform={'uppercase'} fontSize={'1.5rem'} fontWeight={'thin'}>PROFILE</Text>
-        <Box w={'62vw'} mt={'2vh'} display={'flex'} flexDir={'row'} justifyContent={'space-between'}>       
-          <Box border={'2px solid'} rounded={'sm'} borderColor={'gray.200'} w={'40vw'} h={'50vh'}>
+      <Box pl={"5vw"}  w={"70vw"}>
+        <Text textTransform={"uppercase"} fontSize={"1.5rem"} fontWeight={"thin"}>PROFILE</Text>
+        <Box w={"62vw"} mt={"2vh"} display={"flex"} flexDir={"row"} justifyContent={"space-between"}>       
+          <Box border={"2px solid"} rounded={"sm"} borderColor={"gray.200"} w={"40vw"} h={"50vh"}>
           </Box>
           <SideCard/>
+        </Box>
       </Box>
-  </Box>
 
 
     </>
   );
-}
+};
 
-export default ProfileInfo
+export default ProfileInfo;
