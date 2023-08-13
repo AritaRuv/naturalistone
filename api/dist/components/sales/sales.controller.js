2927eb332292b725b0c9632a0317faa22fb462ad
Eduardo Sequeira <eduardoasm19@gmail.com>
2023-08-13 18:45:26 -0400

    feature: route saledetails, button in products for add favorites

--- api/dist/components/sales/sales.controller.js
+++ api/dist/components/sales/sales.controller.js
@@ -12,12 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSalesByProject = exports.getSales = void 0;
+exports.getDetailOfSale = exports.getSalesByUser = exports.getSalesByProject = exports.getSales = void 0;
 const db_1 = __importDefault(require("../../db"));
 function getSales(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const query = `SELECT Sales.* FROM NaturaliStone.Sales`;
+            const query = `SELECT Sales.* FROM Sales`;
             db_1.default.query(query, (error, results, fields) => {
                 if (error) {
                     throw error;
@@ -70,3 +70,100 @@ function getSalesByProject(req, res) {
     });
 }
 exports.getSalesByProject = getSalesByProject;
+function getSalesByUser(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { id } = req.params;
+        try {
+            const _query = `SELECT Sales.*, Projects.ProjectName, Projects.Active, Projects.CustomerID
+    FROM Customers
+    LEFT JOIN Projects ON Projects.CustomerID = Customers.CustomerID
+    LEFT JOIN Sales ON Sales.ProjectID = Projects.idProjects
+    WHERE Customers.CustomerID = ${id}`;
+            db_1.default.query(_query, function (err, results) {
+                if (err) {
+                    return res.status(400).json({
+                        success: false,
+                        msg: "Error in get sales by customer",
+                        error: err,
+                    });
+                }
+                if (!results.length) {
+                    return res
+                        .status(404)
+                        .json({ success: false, msg: "sales not found", data: results });
+                }
+                return res
+                    .status(200)
+                    .json({ success: true, msg: "Sales data successful", data: results });
+            });
+        }
+        catch (error) {
+            console.log(error);
+            return res.status(500).json({ success: false, msg: "General error" });
+        }
+    });
+}
+exports.getSalesByUser = getSalesByUser;
+function getDetailOfSale(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { id } = req.params;
+        try {
+            const _query = `SELECT DISTINCT Sales.* FROM Sales WHERE Naturali_Invoice = ${id}`;
+            db_1.default.query(_query, function (err, resultsSales) {
+                if (err) {
+                    return res.status(400).json({
+                        success: false,
+                        msg: "Error in get detail of sale",
+                        error: err,
+                    });
+                }
+                if (!resultsSales.length) {
+                    return res
+                        .status(404)
+                        .json({ success: false, msg: "Details of sale not found" });
+                }
+                const _query2 = `SELECT Payments.idPayments, Payments.InvoiceID, Payments.Amount, Payments.Method, Payments.Date
+        FROM Payments
+        WHERE InvoiceID = ${id}`;
+                db_1.default.query(_query2, function (err, resultsPayment) {
+                    if (err) {
+                        return res.status(400).json({
+                            success: false,
+                            msg: "Error in get payments of sale",
+                            error: err,
+                        });
+                    }
+                    const _query3 = `SELECT ProdSold.ProdID, ProdSold.Quantity, ProdSold.SalePrice, ProdSold.Status,
+          ProdNames.Naturali_ProdName, ProdNames.Material, ProdNames.ProdNameID FROM Products
+          LEFT JOIN ProdNames ON ProdNames.ProdNameID = Products.ProdNameID
+          LEFT JOIN ProdSold ON ProdSold.ProdID = Products.ProdID
+          WHERE ProdSold.SaleID = ${id}`;
+                    db_1.default.query(_query3, function (err, resultsProdSold) {
+                        if (err) {
+                            return res
+                                .status(400)
+                                .json({ success: false, msg: "Error in get prodSolds" });
+                        }
+                        const details = {
+                            sale: resultsSales[0],
+                            payments: resultsPayment,
+                            prodSolds: resultsProdSold,
+                        };
+                        return res.status(200).json({
+                            success: true,
+                            msg: "Details of sale successful",
+                            data: details,
+                        });
+                    });
+                });
+            });
+        }
+        catch (error) {
+            console.log(error);
+            return res
+                .status(500)
+                .json({ success: false, msg: "General error", error });
+        }
+    });
+}
+exports.getDetailOfSale = getDetailOfSale;
--- api/src/components/sales/sales.controller.ts
+++ api/src/components/sales/sales.controller.ts
@@ -6,7 +6,7 @@ import { RowDataPacket, FieldPacket } from "mysql2";
 
 export async function getSales(req: Request, res: Response) {
   try {
-    const query = `SELECT Sales.* FROM NaturaliStone.Sales`;
+    const query = `SELECT Sales.* FROM Sales`;
 
     mysqlConnection.query(
       query,
@@ -51,6 +51,112 @@ export async function getSalesByProject(req: Request, res: Response) {
             .status(200)
             .json({ success: true, msg: "get data successful", data: results });
         }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, msg: "General error", error });
+  }
+}
+
+export async function getSalesByUser(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const _query = `SELECT Sales.*, Projects.ProjectName, Projects.Active, Projects.CustomerID
+    FROM Customers
+    LEFT JOIN Projects ON Projects.CustomerID = Customers.CustomerID
+    LEFT JOIN Sales ON Sales.ProjectID = Projects.idProjects
+    WHERE Customers.CustomerID = ${id}`;
+
+    mysqlConnection.query(
+      _query,
+      function (err: MysqlError, results: RowDataPacket[]) {
+        if (err) {
+          return res.status(400).json({
+            success: false,
+            msg: "Error in get sales by customer",
+            error: err,
+          });
+        }
+        if (!results.length) {
+          return res
+            .status(404)
+            .json({ success: false, msg: "sales not found", data: results });
+        }
+        return res
+          .status(200)
+          .json({ success: true, msg: "Sales data successful", data: results });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, msg: "General error" });
+  }
+}
+
+export async function getDetailOfSale(req: Request, res: Response) {
+  const { id } = req.params;
+
+  try {
+    const _query = `SELECT DISTINCT Sales.* FROM Sales WHERE Naturali_Invoice = ${id}`;
+
+    mysqlConnection.query(
+      _query,
+      function (err: MysqlError, resultsSales: RowDataPacket) {
+        if (err) {
+          return res.status(400).json({
+            success: false,
+            msg: "Error in get detail of sale",
+            error: err,
+          });
+        }
+        if (!resultsSales.length) {
+          return res
+            .status(404)
+            .json({ success: false, msg: "Details of sale not found" });
+        }
+
+        const _query2 = `SELECT Payments.idPayments, Payments.InvoiceID, Payments.Amount, Payments.Method, Payments.Date
+        FROM Payments
+        WHERE InvoiceID = ${id}`;
+
+        mysqlConnection.query(_query2, function (err, resultsPayment) {
+          if (err) {
+            return res.status(400).json({
+              success: false,
+              msg: "Error in get payments of sale",
+              error: err,
+            });
+          }
+
+          const _query3 = `SELECT ProdSold.ProdID, ProdSold.Quantity, ProdSold.SalePrice, ProdSold.Status,
+          ProdNames.Naturali_ProdName, ProdNames.Material, ProdNames.ProdNameID FROM Products
+          LEFT JOIN ProdNames ON ProdNames.ProdNameID = Products.ProdNameID
+          LEFT JOIN ProdSold ON ProdSold.ProdID = Products.ProdID
+          WHERE ProdSold.SaleID = ${id}`;
+
+          mysqlConnection.query(_query3, function (err, resultsProdSold) {
+            if (err) {
+              return res
+                .status(400)
+                .json({ success: false, msg: "Error in get prodSolds" });
+            }
+
+            const details = {
+              sale: resultsSales[0],
+              payments: resultsPayment,
+              prodSolds: resultsProdSold,
+            };
+
+            return res.status(200).json({
+              success: true,
+              msg: "Details of sale successful",
+              data: details,
+            });
+          });
+        });
       }
     );
   } catch (error) {
