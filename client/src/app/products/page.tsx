4fa5d9da42738018efa576f81b7ce821aeb068a7
Maximiliano Kiss <maximiliano.kiss@yamana.tech>
2023-08-16 12:44:12 -0300

    Agrego lista desplegable en /producs/page.tsx y funcion para ordenar materiales

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { fetchMaterials } from "@/store/products/actionsProducts";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { ProductState } from "@/store/products/typesProducts";
+import { ProductActionTypes, ProductState } from "@/store/products/typesProducts";
 import { useEffect } from "react";
-import { SimpleGrid, Box, Center } from "@chakra-ui/react";
+import { SimpleGrid, Box, Center, Select, HStack, Flex, Spacer, Text } from "@chakra-ui/react";
 import MaterialCard from "./materialCard";
 
 
@@ -12,21 +12,56 @@ export default function Collections() {
   const { materials } = useAppSelector(
     (state: { productReducer: ProductState }) => state.productReducer
   );
-  useEffect(()=>{
-    if(materials.length === 0)dispatch(fetchMaterials());
-  },[]);
+  useEffect(() => {
+    dispatch(fetchMaterials());
+  }, []);
 
-  return(
+  const handleChangeOrderBy = (e) => {
+    filtrar(e.target.value)
+  }
+
+  const filtrar = (criterio: String) => {
+    try {
+      if (criterio === "AZ" || criterio === "")
+        materials.sort()
+      else
+        materials.reverse()
+      dispatch({
+        type: ProductActionTypes.FETCH_MATERIALS,
+        payload: materials,
+      });
+    } catch (error) {
+      dispatch({
+        type: ProductActionTypes.FETCH_PRODUCTS_FAILURE,
+        error: "Error al obtener los product values",
+      });
+    }
+  }
+
+  return (
     <>
+      <Flex p='4'>
+        <Spacer />
+        <HStack>
+          <Text as='h6' size='sm'>
+            order by
+          </Text>
+          <Select placeholder='Select option' name="s" onChange={(e) => handleChangeOrderBy(e)}>
+            <option value='AZ'> A-Z </option>
+            <option value='ZA'> Z-A</option>
+          </Select>
+        </HStack>
+      </Flex >
       <Center>
-        <SimpleGrid w={"100vw"} m={"8vw"} mt={"15vh"} columns={4} spacingY={"30px"}>
+        <SimpleGrid w={"100vw"} m={"8vw"} mt={"4vh"} columns={4} spacingY={"30px"}>
+
           {
-            materials &&(
+            materials && (
               materials.map((material, i) => {
-                if(material !== "Fraanciaaa"){
-                  return(
+                if (material !== "Fraanciaaa") {
+                  return (
                     <Box key={i}>
-                      <MaterialCard material={material}/>
+                      <MaterialCard material={material} />
                     </Box>
                   );
                 }
