c2b975c1fd61fddf076ccb6a0eaf31d4cef4c81a
Maximiliano Kiss <maximiliano.kiss@yamana.tech>
2023-08-24 13:54:18 -0300

    Filtros añidados, tanto en samples como en productos

--- client/src/components/productCard/productValuesList.tsx
+++ client/src/components/productCard/productValuesList.tsx
@@ -4,7 +4,6 @@ import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { postCart } from "@/store/cart/actionsCart";
 import { fetchProductsValuesValidation } from "@/store/products/actionsProducts";
 import { ProductState } from "@/store/products/typesProducts";
-import sizesCheckboxList from "./sizesCheckboxList";
 
 
 interface ProductListProps {
@@ -24,12 +23,13 @@ const ProductList: React.FC<ProductListProps> = ({ data, ProdNameID }) => {
   const dispatch = useAppDispatch();
   const { size, thickness, finish, prodNameID } = data[ProdNameID];
 
-  const { productValuesValidation } = useAppSelector((state: { productReducer: ProductState }) => state.productReducer);
-
   const [selectedSize, setSelectedSize] = useState<string>("");
   const [selectedThickness, setSelectedThickness] = useState<string>("");
   const [selectedFinish, setSelectedFinish] = useState<string>("");
-  const [sizes, setSizes] = useState<string>("");
+  const [sizes, setSizes] = useState<string[]>([]);
+  const [thicknesses, setThicknesses] = useState<string[]>([]);
+  const [finishes, setFinishes] = useState<string[]>([]);
+  const [cantFiltros, setCantFiltros] = useState<number>(0);
 
   const { raw_products } = useAppSelector(
     (state: { productReducer: ProductState }) => state.productReducer
@@ -38,30 +38,128 @@ const ProductList: React.FC<ProductListProps> = ({ data, ProdNameID }) => {
 
 
   const handleCheckboxChange = (value: string, setState: React.Dispatch<React.SetStateAction<string>>, name: string) => {
-    //console.log(raw_products);
 
+    //filtro raw_products por ProdNameID
     const result = raw_products.filter((prod) => prod.ProdNameID === ProdNameID);
-
     setState(prevState => prevState === value ? "" : value);
+
+    //creo un array igual que result 
+    let resultadoFiltrado = result;
+
     if (name === "finish") {
-      const result2 = result.filter((prod) => prod.Finish === value);
-      setSizes("");
-      const aux = [];
-      for (let index = 0; index < result2.length; index++) {
-        const element: string = result2[index].Size;
-        if (element != null)
-          aux.push(element);
-        setSizes(aux);
+      //si se selecciono (checkeo) un finish filtro por el finish seleccionado
+      if (selectedFinish.length === 0) {
+        setCantFiltros(cantFiltros + 1);
+        setSelectedFinish(value);
+        resultadoFiltrado = result.filter((prod) => prod.Finish === value);
+      }
+      else {
+        setSelectedFinish("");
+        setCantFiltros(cantFiltros - 1);
 
       }
-      dispatch(fetchProductsValuesValidation(value, selectedSize, selectedThickness, prodNameID));
+      //si previamente se selecciono un size, el array filtrado o no por finish, se filtra por size
+      if (selectedSize) {
+        resultadoFiltrado = resultadoFiltrado.filter((prod) => prod.Size === selectedSize);
+      }
+      //si previamente se selecciono un thickness, el array filtrado o no por finish, se filtra por thickness
+
+      if (selectedThickness) {
+        resultadoFiltrado = resultadoFiltrado.filter((prod) => prod.Thickness === selectedThickness);
+      }
+
+      //recorro los  productos filtrados y si los sizes no son null
+      // los agrego a la coleccion de sizes disponibles(sizesMatches)
+      //terminado el recorrido los seteo en el estado setSizes
+
+      const sizesMatches: string[] = [];
+      for (let index = 0; index < resultadoFiltrado.length; index++) {
+        const element: string = resultadoFiltrado[index].Size;
+        if (element != null)
+          sizesMatches.push(element);
+        setSizes(sizesMatches);
+      }
+      //lo mismo hago con thickness
+
+      const thicknessMatches: string[] = [];
+      for (let index = 0; index < resultadoFiltrado.length; index++) {
+        const element: string = resultadoFiltrado[index].Thickness;
+        if (element != null)
+          thicknessMatches.push(element);
+        setThicknesses(thicknessMatches);
+      }
+
     }
     if (name === "size") {
-      dispatch(fetchProductsValuesValidation(selectedFinish, value, selectedThickness, prodNameID));
+      if (selectedSize.length === 0) {
+        setSelectedSize(value);
+        setCantFiltros(cantFiltros + 1);
+        resultadoFiltrado = result.filter((prod) => prod.Size === value);
+      }
+      else {
+        setSelectedSize("");
+        setCantFiltros(cantFiltros - 1);
+
+      }
+      if (selectedFinish) {
+        resultadoFiltrado = resultadoFiltrado.filter((prod) => prod.Finish === selectedFinish);
+      }
+      if (selectedThickness) {
+        resultadoFiltrado = resultadoFiltrado.filter((prod) => prod.Thickness === selectedThickness);
+      }
+      setFinishes([]);
+      const aux: string[] = [];
+      for (let index = 0; index < resultadoFiltrado.length; index++) {
+        const element: string = resultadoFiltrado[index].Finish;
+        if (element != null)
+          aux.push(element);
+        setFinishes(aux);
+      }
+      const aux2: string[] = [];
+
+      for (let index = 0; index < resultadoFiltrado.length; index++) {
+        const element: string = resultadoFiltrado[index].Thickness;
+        if (element != null)
+          aux2.push(element);
+        setThicknesses(aux2);
+      }
     }
     if (name === "thickness") {
-      dispatch(fetchProductsValuesValidation(selectedFinish, selectedSize, value, prodNameID));
+
+      if (selectedThickness.length === 0) {
+        setSelectedThickness(value);
+        setCantFiltros(cantFiltros + 1);
+        resultadoFiltrado = result.filter((prod) => prod.Thickness === value);
+      }
+      else {
+        setCantFiltros(cantFiltros - 1);
+        setSelectedThickness("");
+      }
+
+      if (selectedFinish) {
+        resultadoFiltrado = resultadoFiltrado.filter((prod) => prod.Finish === selectedFinish);
+      }
+      if (selectedSize) {
+        resultadoFiltrado = resultadoFiltrado.filter((prod) => prod.Size === selectedSize);
+      }
+      const aux: string[] = [];
+      for (let index = 0; index < resultadoFiltrado.length; index++) {
+        const element: string = resultadoFiltrado[index].Finish;
+        if (element != null)
+          aux.push(element);
+        setFinishes(aux);
+      }
+      const aux2: string[] = [];
+
+      for (let index = 0; index < resultadoFiltrado.length; index++) {
+        const element: string = resultadoFiltrado[index].Size;
+        if (element != null)
+          aux2.push(element);
+        setSizes(aux2);
+      }
     }
+
+
   };
 
   const handleAddToCart = async () => {
@@ -96,6 +194,7 @@ const ProductList: React.FC<ProductListProps> = ({ data, ProdNameID }) => {
                   iconColor="orange"
                   borderColor={"blackAlpha.600"}
                   isChecked={selectedFinish === finish}
+                  isDisabled={finishes.length > 0 && !finishes.includes(finish) && cantFiltros > 0}
                   onChange={() => handleCheckboxChange(finish, setSelectedFinish, "finish")}
                 />
               </Box>
@@ -105,9 +204,6 @@ const ProductList: React.FC<ProductListProps> = ({ data, ProdNameID }) => {
         <CheckboxGroup colorScheme='whiteAlpha' value={[selectedSize]}>
           <VStack align="start" w={"80px"}>
             <Text fontSize='0.7rem' fontWeight={"semibold"}>SIZE</Text>
-            {/* <sizesCheckboxList >
-
-            </sizesCheckboxList> */}
             {size.map(size => (
               <Box key={size} fontSize={"0.7rem"} display={"flex"} justifyContent={"space-between"} w={"60px"}>
                 {size}
@@ -117,7 +213,7 @@ const ProductList: React.FC<ProductListProps> = ({ data, ProdNameID }) => {
                   iconColor="orange"
                   borderColor={"blackAlpha.600"}
                   isChecked={selectedSize === size}
-                  isDisabled={!sizes.includes(size)}
+                  isDisabled={sizes.length > 0 && !sizes.includes(size) && cantFiltros > 0}
                   onChange={() => handleCheckboxChange(size, setSelectedSize, "size")}
                 />
               </Box>
@@ -137,6 +233,7 @@ const ProductList: React.FC<ProductListProps> = ({ data, ProdNameID }) => {
                   borderColor={"blackAlpha.600"}
                   isChecked={selectedThickness === thickness}
                   onChange={() => handleCheckboxChange(thickness, setSelectedThickness, "thickness")}
+                  isDisabled={thicknesses.length > 0 && !thicknesses.includes(thickness) && cantFiltros > 0}
                 />
               </Box>
             ))}
