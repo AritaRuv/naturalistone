ab3fb430bd98f3352a2fc813f1aedbec59b8872c
Maximiliano Kiss <maximiliano.kiss@yamana.tech>
2023-09-28 11:44:21 -0300

    post address

--- api/src/components/address/address.routes.ts
+++ api/src/components/address/address.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllCustomerAddress
+import { getAllCustomerAddress, postNewAddress
 } from "./address.controller";
 
 const addressRouter: Router = Router();
 
 addressRouter.get("/get_all", getAllCustomerAddress);
+addressRouter.post("/create", postNewAddress);
 
 
 export default addressRouter;
--- client/src/api/apiAddress.ts
+++ client/src/api/apiAddress.ts
@@ -1,4 +1,5 @@
 // api.ts
+import { Address } from "@/interfaces/Address";
 import axios from "axios";
 import Cookies from "js-cookie";
 
@@ -22,5 +23,27 @@ export const getAllAddressByCusomer = async () => {
   } catch (error) {
     console.log(error);
     throw new Error("Error al obtener favorites de la API");
+  }
+};
+
+export const createAddress = async (
+  bodyAddress: Address,
+) => {
+  try {
+    const token: string | undefined = Cookies.get("sessionId");
+    if (!token) {
+      throw new Error("No token available");
+    }
+    const response = await axios.post(
+      "http://localhost:5000/api/projects/create", bodyAddress, {
+      headers: {
+        authorization: token,
+      },
+    });
+    console.log("api", { response });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al crear project en apiProjects");
   }
 };
